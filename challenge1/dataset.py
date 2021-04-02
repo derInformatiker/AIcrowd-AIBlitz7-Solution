fab405514c4594e23755f60df8d7e3300c253406
derInformatiker <68816018+derInformatiker@users.noreply.github.com>
2021-04-02 10:39:11 +0200

    Add files via upload

--- challenge1/dataset.py
+++ challenge1/dataset.py
@@ -14,7 +14,7 @@ class ImgDataset(Dataset):
     def __getitem__(self,x):
         path = self.imageID.iloc[x]
         label = float(self.labelmap[self.labels.iloc[x]])
-        i = cv2.imread(f'D:/rover/ch1/data/{self.mode}/'+str(path)+'.jpg')
+        i = cv2.imread(f'data/{self.mode}/'+str(path)+'.jpg')
         i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
         
         if self.transforms:
@@ -32,17 +32,17 @@ class ImgDataset(Dataset):
         return len(self.imageID)
     
 def getTrainDs(train_tr = None):
-    train_df = pd.read_csv('D:/rover/ch1/data/train.csv')
+    train_df = pd.read_csv('data/train.csv')
     return ImgDataset(train_df,'train',train_tr)
 
 def getValDs(val_tr):
-    val_df = pd.read_csv('D:/rover/ch1/data/val.csv')
+    val_df = pd.read_csv('data/val.csv')
     return ImgDataset(val_df,'val',val_tr)
 
 def getTestDs(test_tr):
-    val_df = pd.read_csv('D:/rover/ch1/data/sample_submission.csv')
+    val_df = pd.read_csv('data/sample_submission.csv')
     return ImgDataset(val_df,'test',test_tr)
 
 def writeSub(p):
-    test_df = pd.read_csv('D:/rover/ch1/data/sample_submission.csv')
+    test_df = pd.read_csv('data/sample_submission.csv')
     
